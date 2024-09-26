29b9fdbc9218fe3cb5027f308296b88c14fb8876
shivamknjvs <shivam.knjvs9@gmail.com>
2024-09-26 17:47:17 +0530

    commit

--- backend/app.js
+++ backend/app.js
@@ -17,7 +17,7 @@ app.use(express.json());
 app.use(express.urlencoded({extended:true}));
 app.use(cookieParser());
 const corsOptions = {
-    origin:'https://job-portal-one-ebon.vercel.app/',
+    origin:'https://job-portal-one-ebon.vercel.app',
     credentials:true
 }
 
