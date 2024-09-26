ab80a2428cefd5a432a79e4e1a0821fbd8258c03
shivamknjvs <shivam.knjvs9@gmail.com>
2024-09-26 17:42:13 +0530

    COMMIT

--- backend/app.js
+++ backend/app.js
@@ -16,9 +16,12 @@ const app = express();
 app.use(express.json());
 app.use(express.urlencoded({extended:true}));
 app.use(cookieParser());
+const corsOptions = {
+    origin:'https://job-portal-one-ebon.vercel.app/',
+    credentials:true
+}
 
-
-app.use(cors());
+app.use(cors(corsOptions));
 
 const PORT = process.env.PORT || 3000;
 
