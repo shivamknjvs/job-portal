1d4fe8b9ec14fdf9a9aaafe6994451e93183c3f4
shivamknjvs <99579623+shivamknjvs@users.noreply.github.com>
2024-09-26 22:30:37 +0530

    Update user.controller.js

--- backend/controllers/user.controller.js
+++ backend/controllers/user.controller.js
@@ -92,7 +92,7 @@ export const login = async (req, res) => {
             profile: user.profile
         }
 
-        return res.status(200).cookie("token", token, { maxAge: 1 * 24 * 60 * 60 * 1000, httpsOnly: true, sameSite: 'strict' , secure: true }).json({
+        return res.status(200).cookie("token", token, { maxAge: 1 * 24 * 60 * 60 * 1000, httpsOnly: true, sameSite: 'none' , secure: true }).json({
             message: `Welcome back ${user.fullname}`,
             user,
             success: true
